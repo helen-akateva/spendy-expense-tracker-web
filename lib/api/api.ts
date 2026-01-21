a8bdacfa17ad885b7cd336dd84566d1641d023ea
Anton Shuvalov <anshuvalov83@gmail.com>
2026-01-21 17:58:45 +0200

    fix(auth): registration submit, loader and redirect

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,15 +2,10 @@ import axios, { AxiosError } from "axios";
 
 export type ApiError = AxiosError<{ error: string }>;
 
-const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ||
-  "http://localhost:3002" ||
-  "http://localhost:3001";
-
 export const nextApi = axios.create({
-  baseURL: API_URL,
-    withCredentials: true,
-   headers: {
+  baseURL: "",
+  withCredentials: true,
+  headers: {
     "Content-Type": "application/json",
   },
 });
