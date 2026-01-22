4fe2ffc272bb59e1f7a358e33590a3143afb0e2a
helen-akateva <akateva17@gmail.com>
2026-01-22 13:54:14 +0200

    fix

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,10 +2,7 @@ import axios, { AxiosError } from "axios";
 
 export type ApiError = AxiosError<{ error: string }>;
 
-const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ||
-  "http://localhost:3002" ||
-  "http://localhost:3001";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const nextApi = axios.create({
   baseURL: API_URL + "/api",
